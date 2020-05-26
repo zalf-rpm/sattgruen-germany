f9e62f8f9951b3947c54d635e240437e1ad747ec
kamaliba <35263355+kamaliba@users.noreply.github.com>
2020-05-26 19:13:06 +0200

    cut5

--- run-producer.py
+++ run-producer.py
@@ -89,7 +89,7 @@ PATHS = {
 
 DEFAULT_HOST = "localhost"
 DEFAULT_PORT = "6666"
-RUN_SETUP = "[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]"
+RUN_SETUP = "[1,2,3,4,5,6,7,8,9,10,11,12]"
 SETUP_FILE = "sim_setups.csv"
 DATA_SOIL_DB = "germany/buek1000.sqlite"
 DATA_GRID_HEIGHT = "germany/dem_1000_gk5.asc" 
